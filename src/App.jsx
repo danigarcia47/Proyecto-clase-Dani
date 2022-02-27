0ae4fd53953037a7c1e415b902cdafc43d94dca3
danigarcia47 <dgarcial01@educantabria.es>
2022-02-27 09:59:14 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import BootstrapHeader from '.components/BootstrapHeader';
+import { Menu } from '.data/Menu';
+
+export function App() {
+  return (
+    <Router>
+      {Menu.map((item) => {
+        return (
+          <Route
+            key = {item.id}
+            path = {item.path}
+            exact
+            component = {item.component}
+          />
+        )
+      })}
+    </Router>
+  );
+}
---
+++ src/data/Menu.js
@@ -0,0 +1,10 @@
+import UserSesion from '../components/UserSesion';
+
+export const Menu = [
+  {
+    id: 1,
+    path: '/usersesion',
+    title: 'Inicio Sesión',
+    component: UserSesion,
+  },
+]
